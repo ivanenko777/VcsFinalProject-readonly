58e9104b775b0a78c299fa234dffb5307e6e5357
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-09-22 20:21:55 +0300

    Create: * registration, login forms * AuthController with login(), registration() and register() * CustomerService with create() * UsernameExistsInDatabaseException

---
+++ web-externalApp/src/main/java/lt/ivl/webExternalApp/service/CustomerService.java
@@ -0,0 +1,21 @@
+package lt.ivl.webExternalApp.service;
+
+import lt.ivl.webExternalApp.domain.Customer;
+import lt.ivl.webExternalApp.exception.UsernameExistsInDatabaseException;
+import lt.ivl.webExternalApp.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public void create(Customer customer) throws UsernameExistsInDatabaseException {
+        Customer customerFromDb = customerRepository.findByEmail(customer.getEmail());
+        if (customerFromDb != null) throw new UsernameExistsInDatabaseException("User exists in DB");
+
+        customer.setActive(true);
+        customerRepository.save(customer);
+    }
+}
