b8c385573f77e9013fbf417f569d0eb2bdf31f7a
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-10-03 23:48:21 +0300

    add @ComponentScan annotation

--- web-externalApp/src/main/java/lt/ivl/webExternalApp/ExternalApplication.java
+++ web-externalApp/src/main/java/lt/ivl/webExternalApp/ExternalApplication.java
@@ -3,6 +3,7 @@ package lt.ivl.webExternalApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @EntityScan(basePackages = {"lt.ivl.components.domain"})
 @EnableJpaRepositories(basePackages = {"lt.ivl.components.repository"})
+@ComponentScan(basePackages = {"lt.ivl.components", "lt.ivl.webExternalApp"})
 public class ExternalApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExternalApplication.class);
