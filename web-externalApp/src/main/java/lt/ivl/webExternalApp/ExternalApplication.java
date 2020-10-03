2380fe444a6f6fcb26a4733c2ac1c8718e5cd129
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-10-03 23:13:10 +0300

    add @EnableJpaRepositories annotation

--- web-externalApp/src/main/java/lt/ivl/webExternalApp/ExternalApplication.java
+++ web-externalApp/src/main/java/lt/ivl/webExternalApp/ExternalApplication.java
@@ -3,11 +3,13 @@ package lt.ivl.webExternalApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 @EntityScan(basePackages = {"lt.ivl.components.domain"})
+@EnableJpaRepositories(basePackages = {"lt.ivl.components.repository"})
 public class ExternalApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExternalApplication.class);
