118ea24acb4ed3ad226e6d0d4b25764b4beb795c
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-10-03 23:07:57 +0300

    add @EntityScan annotation

--- web-externalApp/src/main/java/lt/ivl/webExternalApp/ExternalApplication.java
+++ web-externalApp/src/main/java/lt/ivl/webExternalApp/ExternalApplication.java
@@ -2,10 +2,12 @@ package lt.ivl.webExternalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EntityScan(basePackages = {"lt.ivl.components.domain"})
 public class ExternalApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExternalApplication.class);
