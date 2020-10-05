01369ffb5ebb7f91845f381fa6f282844173399a
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-10-05 18:00:05 +0300

    create Employee entity, repository and service

---
+++ components/src/main/java/lt/ivl/components/domain/Employee.java
@@ -0,0 +1,74 @@
+package lt.ivl.components.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private boolean active;
+
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    public Employee() {
+        this.active = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ components/src/main/java/lt/ivl/components/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package lt.ivl.components.service;
+
+import lt.ivl.components.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    EmployeeRepository employeeRepository;
+}
