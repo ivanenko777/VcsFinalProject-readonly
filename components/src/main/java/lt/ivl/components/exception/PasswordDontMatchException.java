d92220a491f31702fadcf687d2bc13e8c4a3955f
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-10-06 00:00:31 +0300

    move PasswordDontMatchException webExternalApp -> components module

--- components/src/main/java/lt/ivl/components/exception/PasswordDontMatchException.java
+++ components/src/main/java/lt/ivl/components/exception/PasswordDontMatchException.java
@@ -1,4 +1,4 @@
-package lt.ivl.webExternalApp.exception;
+package lt.ivl.components.exception;
 
 public class PasswordDontMatchException extends Exception {
     public PasswordDontMatchException() {
