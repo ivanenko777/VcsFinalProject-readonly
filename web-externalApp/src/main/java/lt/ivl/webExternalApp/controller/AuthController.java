4fee2dd5ed470a312ee9de93c906d73ae3efcfb6
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-10-06 00:01:24 +0300

    optimize imports

--- web-externalApp/src/main/java/lt/ivl/webExternalApp/controller/AuthController.java
+++ web-externalApp/src/main/java/lt/ivl/webExternalApp/controller/AuthController.java
@@ -3,6 +3,7 @@ package lt.ivl.webExternalApp.controller;
 import lt.ivl.components.domain.Customer;
 import lt.ivl.components.domain.CustomerResetPasswordToken;
 import lt.ivl.components.domain.CustomerVerificationToken;
+import lt.ivl.components.exception.PasswordDontMatchException;
 import lt.ivl.components.exception.TokenExpiredException;
 import lt.ivl.components.exception.TokenInvalidException;
 import lt.ivl.webExternalApp.dto.CustomerDto;
