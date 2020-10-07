47e356adbff9f0d956c9722e5b99098b435e2994
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-10-07 15:40:17 +0300

    implement employee view() method in ManageEmployeeController

--- web-internalApp/src/main/java/lt/ivl/webInternalApp/controller/ManageEmployeeController.java
+++ web-internalApp/src/main/java/lt/ivl/webInternalApp/controller/ManageEmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,5 +22,11 @@ public class ManageEmployeeController {
         List<Employee> employeeList = internalEmployeeService.findAll();
         model.addAttribute("employeeList", employeeList);
         return "manage-employee/list";
+    }
+
+    @GetMapping("/{employee}/view")
+    public String view(@PathVariable("employee") Employee employee, Model model) {
+        model.addAttribute("employee", employee);
+        return "manage-employee/view";
     }
 }
