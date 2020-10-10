b7408e54659e14fcad06b510e49683e3128dc7b1
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-10-10 20:33:08 +0300

    refactor webExternalApp MailSender * extract constructEmail

--- web-externalApp/src/main/java/lt/ivl/webExternalApp/service/MailSender.java
+++ web-externalApp/src/main/java/lt/ivl/webExternalApp/service/MailSender.java
@@ -41,70 +41,63 @@ public class MailSender {
         mailSender.send(email);
     }
 
-    private SimpleMailMessage constructCustomerVerificationEmail(Customer customer, String token) {
-        final String recipientAddress = customer.getEmail();
-        final String subject = "Registracijos patvirtinimas";
-        final String appUrl = environment.getProperty("app.url");
-        final String confirmationUrl = appUrl + "activation?token=" + token;
-        final String message1 = String.format("Sveiki, %s %s,", customer.getFirstName(), customer.getLastName());
-        final String message2 = "Jūs sėkmingai užsiregistravote. Paspauskite žemiau esančią nuorodą, kad patvirtintumėte registraciją.";
-
+    private SimpleMailMessage constructEmail(String recipientAddress, String subject, String message) {
         final SimpleMailMessage email = new SimpleMailMessage();
         email.setFrom(environment.getProperty("support.email"));
         email.setTo(recipientAddress);
         email.setSubject(subject);
-        email.setText(message1 + " \r\n" + message2 + " \r\n" + confirmationUrl);
+        email.setText(message);
         return email;
+    }
+
+    private SimpleMailMessage constructCustomerVerificationEmail(Customer customer, String token) {
+        final String message1 = String.format("Sveiki, %s %s,", customer.getFirstName(), customer.getLastName());
+        final String message2 = "Jūs sėkmingai užsiregistravote. Paspauskite žemiau esančią nuorodą, kad patvirtintumėte registraciją.";
+        final String appUrl = environment.getProperty("app.url");
+        final String confirmationUrl = appUrl + "activation?token=" + token;
+
+        final String recipientAddress = customer.getEmail();
+        final String subject = "Registracijos patvirtinimas";
+        final String message = message1 + " \r\n" + message2 + " \r\n" + confirmationUrl;
+        return constructEmail(recipientAddress, subject, message);
     }
 
     private SimpleMailMessage constructCustomerActivatatedEmail(Customer customer) {
-        final String recipientAddress = customer.getEmail();
-        final String subject = "Registracija patvirtinta";
-        final String appUrl = environment.getProperty("app.url");
-        final String loginUrl = appUrl + "login";
         final String message1 = String.format("Sveiki, %s %s,", customer.getFirstName(), customer.getLastName());
         final String message2 = "Jūsų registracija patvirtinta. Paspauskite žemiau esančią nuorodą, kad prisijungtumėte.";
+        final String appUrl = environment.getProperty("app.url");
+        final String loginUrl = appUrl + "login";
 
-        final SimpleMailMessage email = new SimpleMailMessage();
-        email.setFrom(environment.getProperty("support.email"));
-        email.setTo(recipientAddress);
-        email.setSubject(subject);
-        email.setText(message1 + " \r\n" + message2 + " \r\n" + loginUrl);
-        return email;
+        final String recipientAddress = customer.getEmail();
+        final String subject = "Registracija patvirtinta";
+        final String message = message1 + " \r\n" + message2 + " \r\n" + loginUrl;
+        return constructEmail(recipientAddress, subject, message);
     }
 
     private SimpleMailMessage constructCustomerResetPasswordEmail(Customer customer, String token) {
-        final String recipientAddress = customer.getEmail();
-        final String subject = "Slaptažodžio pakeitimas";
         final String appUrl = environment.getProperty("app.url");
         final String resetPasswordUrl = appUrl + "reset-password?token=" + token;
         final String message1 = String.format("Sveiki, %s %s,", customer.getFirstName(), customer.getLastName());
         final String message2 = "Paspauskite žemiau esančią nuorodą, kad pakeisti slaptažodį.";
 
-        final SimpleMailMessage email = new SimpleMailMessage();
-        email.setFrom(environment.getProperty("support.email"));
-        email.setTo(recipientAddress);
-        email.setSubject(subject);
-        email.setText(message1 + " \r\n" + message2 + " \r\n" + resetPasswordUrl);
-        return email;
+        final String recipientAddress = customer.getEmail();
+        final String subject = "Slaptažodžio pakeitimas";
+        final String message = message1 + " \r\n" + message2 + " \r\n" + resetPasswordUrl;
+        return constructEmail(recipientAddress, subject, message);
     }
 
     private SimpleMailMessage constructCustomerRepairRequestEmail(Customer customer, Repair repair) {
         // remonto paraiska, STATUS -> PENDING
         final int repairId = repair.getId();
 
-        final String recipientAddress = customer.getEmail();
-        final String subject = "Užsakymo paraiška #" + repairId;
         final String appUrl = environment.getProperty("app.url");
         final String repairViewUrl = appUrl + "repair/" + repairId + "/view";
         final String message1 = String.format("Sveiki, %s %s,", customer.getFirstName(), customer.getLastName());
         final String message2 = "Remonto paraiška užregistruota. Sekite info žemiau esančioje nuorodoje.";
 
-        final SimpleMailMessage email = new SimpleMailMessage();
-        email.setFrom(environment.getProperty("support.email"));
-        email.setTo(recipientAddress);
-        email.setSubject(subject);
-        email.setText(message1 + " \r\n" + message2 + " \r\n" + repairViewUrl);
-        return email;
+        final String recipientAddress = customer.getEmail();
+        final String subject = "Užsakymo paraiška #" + repairId;
+        final String message = message1 + " \r\n" + message2 + " \r\n" + repairViewUrl;
+        return constructEmail(recipientAddress, subject, message);
     }
 }
