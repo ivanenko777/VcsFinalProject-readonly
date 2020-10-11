ce17fed222345152f60d0f982cc9e6dd40a3b8f9
Vladislav <1019949+ivanevla@users.noreply.github.com>
2020-10-12 00:56:21 +0300

    implement view() method in ManageRepairsController

--- web-internalApp/src/main/java/lt/ivl/webInternalApp/controller/ManageRepairController.java
+++ web-internalApp/src/main/java/lt/ivl/webInternalApp/controller/ManageRepairController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,5 +22,11 @@ public class ManageRepairController {
         List<Repair> repairList = internalRepairService.findAll();
         model.addAttribute("repairList", repairList);
         return "repair/list";
+    }
+
+    @GetMapping("/{repair}/view")
+    public String view(@PathVariable("repair") Repair repair, Model model) {
+        model.addAttribute("repair", repair);
+        return "repair/view";
     }
 }
